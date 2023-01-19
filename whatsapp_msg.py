4953507f7052daec854709cfce0fd8474dfe91bd
Arun Kumar <112628113+Akgoldie@users.noreply.github.com>
2023-01-19 23:26:26 +0530

    Add files via upload

---
+++ whatsapp_msg.py
@@ -0,0 +1,12 @@
+import random
+
+import pyautogui as pg
+import time
+time.sleep(10)
+for i in range (100):
+    pg.write("Hi I am Arun kumar")
+    #pg.write("You
+    # Your Mobile r Phone Is Hacked ☠")
+    #pg.write(emoji.emojize('Your Mobile will be Hacked  ☠'))
+    #pg.write(emoji.emojize("Your Mobile will be Hacked"))
+    pg.press('enter')
